f5cb2c4bb8f8dcb8720a6aaeb44e73e1b9bea7f2
chimamario <mariochima646@gmail.com>
2024-09-10 11:51:40 -0600

    initial commit

---
+++ traffic_flow.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#notes about csv files so far
+
+#training and test data sets
+
+#there are 35 different locations
+#each location refers to 1261 timestamps (each time stamp are 15 min intervals)
+#ex - one row refers to a location, at a specific timestamp. it has the traffic rate at that point as well as 
+
+training_set = pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/machine learning practice/traffic flow/train.csv")
+
+print(training_set.head())
+print(training_set['location'].unique())
