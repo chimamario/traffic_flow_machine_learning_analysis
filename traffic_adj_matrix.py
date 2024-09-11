b444e1fd30661807da7da0b8fc7bcbbea5fd40f1
chimamario <mariochima646@gmail.com>
2024-09-11 09:16:46 -0600

    implemented network x on adjacent matrix

---
+++ traffic_adj_matrix.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+
+adj_matrix = pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/machine learning practice/traffic flow/adj_matrix.csv", index_col=0)
+
+adj_matrix_np = adj_matrix.to_numpy()
+
+G = nx.from_numpy_array(adj_matrix_np)
+
+degree_centrality = nx.degree_centrality(G) # Degree centrality (returns a dictionary where keys are nodes and values are centrality scores)
+clustering_coefficient = nx.clustering(G) # Clustering coefficient (measures the degree to which nodes cluster together)
+eigenvector_centrality = nx.eigenvector_centrality(G) # Eigenvector centrality (measure of node influence)
+shortest_paths = dict(nx.shortest_path_length(G)) # Shortest paths (returns the lengths of the shortest paths between nodes)
+betweenness_centrality = nx.betweenness_centrality(G) # Betweenness centrality (measures the amount of control a node has over communication between pairs of other nodes)
+connected_components = list(nx.connected_components(G)) # Connected components (returns a list of connected components)
+
+component_sizes = {node: len(component) for component in connected_components for node in component}
+connected_component_size_list = [component_sizes[node] for node in G.nodes()]
+
+# Combine these features into a DataFrame or use them as inputs to a machine learning model
+graph_features = pd.DataFrame({
+    'degree_centrality': degree_centrality.values(), #36 values
+    'clustering_coefficient': clustering_coefficient.values(), #36 values
+    'eigenvector_centrality': eigenvector_centrality.values(), #36 values
+    'betweenness_centrality': betweenness_centrality.values(), #36 values
+    'connected_component_size': connected_component_size_list,
+})
+
+# # Print or save features to CSV
+print(graph_features)
--- traffic_flow.py
+++ traffic_flow.py
@@ -1,17 +1,25 @@
 import pandas as pd
+import numpy as np
+import networkx as nx
 
 #notes about csv files so far
 
 #training and test data sets
 
-#there are 35 different locations
+#there are 36 different locations
 #each location refers to 1261 timestamps (each time stamp are 15 min intervals)
 #ex - one row refers to a location, at a specific timestamp. it has the traffic rate at that point as well as 
 
 training_set = pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/machine learning practice/traffic flow/train.csv")
 
-print(training_set.head())
-print(training_set['location'].unique())
+
+pd.set_option('display.max_rows', None)  # Show all rows
+pd.set_option('display.max_columns', None) 
+# print(training_set.head())
+# print(training_set.columns)
 
 #testing
 #testing 2
+
+
+# # graph_features.to_csv('graph_features.csv', index=False)
