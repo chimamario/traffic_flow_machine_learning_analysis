fe55767976047a1c08e431e526e98500446c3ced
chimamario <mariochima646@gmail.com>
2024-09-13 11:05:02 -0600

    cleaned up lstm traffic columm

--- lstm_analysis.py
+++ lstm_analysis.py
@@ -9,7 +9,14 @@ pd.set_option('display.max_columns', None)
 # print(training_set.head())
 
 lstm_data = training_set.iloc[:, 0: 13]
+print(lstm_data.columns)
+
+# lstm_data.rename(columns = )
+lstm_data.columns = ['timestep', 'location', 'traffic_old', 'traffic','prev_1', 'prev_2', 'prev_3',
+       'prev_4', 'prev_5', 'prev_6', 'prev_7', 'prev_8', 'prev_9']
+
 # print(lstm_data.head())
+lstm_data = lstm_data.drop('traffic_old', axis = 1)
 
 #there are 36 different locations
 #each location refers to 1261 timestamps (each time stamp are 15 min intervals)
@@ -17,8 +24,11 @@ lstm_data = training_set.iloc[:, 0: 13]
 #each location has 1261 timestamps - you need to group each dataframe with their timestep and location
 
 lstm_data_jan = lstm_data[lstm_data['location'] == 23]
-print(lstm_data_jan.head(20))
+print(lstm_data_jan.head(5))
 
 #traffic values do no align with prev values so I am going to make prev_1 the traffic value, adjust the other columns, and then remove traffic
 #clean up data (making each dataframe for each location and then organizing them in a dictionary)
 #cont to reshape data and then use pytorch
+
+
+
